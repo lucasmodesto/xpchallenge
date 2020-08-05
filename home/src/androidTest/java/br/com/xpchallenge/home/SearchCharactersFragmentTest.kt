c050554f928f113fe6e6812458f8054af9e16b3a
Lucas Modesto <dev.lucasmodesto@gmail.com>
2020-08-05 15:47:28 -0300

    added search fragment success scenario test

--- home/src/androidTest/java/br/com/xpchallenge/home/SearchCharactersFragmentTest.kt
+++ home/src/androidTest/java/br/com/xpchallenge/home/SearchCharactersFragmentTest.kt
@@ -1,9 +1,13 @@
 package br.com.xpchallenge.home
 
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.ViewMatchers.*
 import br.com.xpchallenge.data.di.RepositoryModule
 import br.com.xpchallenge.domain.entity.CharactersResult
 import br.com.xpchallenge.domain.errors.NetworkError
 import br.com.xpchallenge.domain.repository.ICharacterRepository
+import br.com.xpchallenge.testcommon.atPosition
 import br.com.xpchallenge.testcommon.checkIfViewShowsText
 import br.com.xpchallenge.testcommon.launchFragmentInHiltContainer
 import dagger.hilt.android.testing.BindValue
@@ -13,6 +17,7 @@ import dagger.hilt.android.testing.UninstallModules
 import io.mockk.MockKAnnotations
 import io.mockk.every
 import io.mockk.impl.annotations.MockK
+import io.mockk.mockk
 import io.reactivex.rxjava3.core.Observable
 import io.reactivex.rxjava3.core.Single
 import org.junit.Before
@@ -37,7 +42,27 @@ class SearchCharactersFragmentTest {
     }
 
     @Test
-    fun shouldShowEmptyStateMessage() {
+    fun successScenario() {
+        every { repository.getCharacters(any(), any()) } returns Single.just(
+            CharactersResult(
+                characters = listOf(mockk(relaxed = true){
+                    every { name } returns "Hulk"
+                    every { imageUrl } returns "https://i.annihil.us/u/prod/marvel/i/mg/5/a0/538615ca33ab0.jpg"
+                    every { isImageAvailable } returns true
+                }),
+                count = 0,
+                totalCount = 0,
+                offset = 0))
+        every { repository.getFavoriteCharacters() } returns Observable.just(emptyList())
+
+        launchFragmentInHiltContainer<SearchCharactersFragment>()
+
+        onView(withId(R.id.search_characters_recycler_view))
+            .check(matches(atPosition(0, hasDescendant(withText("Hulk")))))
+    }
+
+    @Test
+    fun emptyStateScenario() {
         every { repository.getCharacters(any(), any()) } returns Single.just(
             CharactersResult(
                 characters = emptyList(),
@@ -52,7 +77,7 @@ class SearchCharactersFragmentTest {
     }
 
     @Test
-    fun shouldShowNoInternetMessage() {
+    fun noInternetScenario() {
         every { repository.getCharacters(any(), any()) } returns Single.error(
             NetworkError.NotConnected()
         )
@@ -64,7 +89,7 @@ class SearchCharactersFragmentTest {
     }
 
     @Test
-    fun shouldShowUnknownErrorMessage() {
+    fun unknownErrorScenario() {
         every { repository.getCharacters(any(), any()) } returns Single.error(Exception())
         every { repository.getFavoriteCharacters() } returns Observable.just(emptyList())
 
--- testcommon/src/main/java/br/com/xpchallenge/testcommon/EspressoUtils.kt
+++ testcommon/src/main/java/br/com/xpchallenge/testcommon/EspressoUtils.kt
@@ -1,11 +1,33 @@
 package br.com.xpchallenge.testcommon
 
+import android.view.View
+import androidx.annotation.NonNull
 import androidx.annotation.StringRes
+import androidx.recyclerview.widget.RecyclerView
 import androidx.test.espresso.Espresso
 import androidx.test.espresso.assertion.ViewAssertions
+import androidx.test.espresso.matcher.BoundedMatcher
 import androidx.test.espresso.matcher.ViewMatchers
+import org.hamcrest.Description
+import org.hamcrest.Matcher
+
 
 fun checkIfViewShowsText(@StringRes message: Int) {
     Espresso.onView(ViewMatchers.withText(message))
         .check(ViewAssertions.matches(ViewMatchers.withEffectiveVisibility(ViewMatchers.Visibility.VISIBLE)))
+}
+
+fun atPosition(position: Int, @NonNull itemMatcher: Matcher<View?>): Matcher<View?>? {
+    return object : BoundedMatcher<View?, RecyclerView>(RecyclerView::class.java) {
+        override fun describeTo(description: Description) {
+            description.appendText("has item at position $position: ")
+            itemMatcher.describeTo(description)
+        }
+
+        override fun matchesSafely(view: RecyclerView): Boolean {
+            val viewHolder: RecyclerView.ViewHolder =
+                view.findViewHolderForAdapterPosition(position) ?: return false
+            return itemMatcher.matches(viewHolder.itemView)
+        }
+    }
 }
